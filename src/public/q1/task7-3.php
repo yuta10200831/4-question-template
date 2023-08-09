6cc07747fe2aee94e44da584c5facac4eb6f8982
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-08-09 22:42:45 +0900

    7. よくあるロジックにトライしよう③　課題③　提出

--- src/public/q1/task7-3.php
+++ src/public/q1/task7-3.php
@@ -8,6 +8,6 @@ $sortedIncomes = $incomes->fetchAndSortByMonthTotal();
 echo "収入が高い順にsortして月ごとの収入の合計を一覧表示";
 echo "<br>";
 foreach ($sortedIncomes as $income) {
-    echo $income["month"] . "月" . $income["total"] . "<br>";
+    echo $income["month"] . "月:" . $income["total"] . "<br>";
 }
 ?>
