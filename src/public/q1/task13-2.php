3d0ff0375ae622d6ee3148e7b7dc948a67ab43bf
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-08-22 22:27:45 +0900

    13.サブスクエリを使ってみよう　課題②　提出

--- src/public/q1/task13-2.php
+++ src/public/q1/task13-2.php
@@ -9,9 +9,12 @@ foreach ($calculateSideIncomeProfits as $data)
 {
     $year = $data['year'];
     $month = str_pad($data['month'], 2, '0', STR_PAD_LEFT);
-    $profit = $data['total_income'] - $data['total_expenses'];
+    $profit = $data['total_income'] - $data['total_expenditure'];
     
-    echo "{$year}-{$month}の「副業の利益」: {$profit}";
+    // 小数点第一位まで表示する
+    $formattedProfit = number_format($profit, 1, '.', '');
+
+    echo "{$year}-{$month}の「副業の利益」: {$formattedProfit}";
     echo "<br>";
 }
 ?>
